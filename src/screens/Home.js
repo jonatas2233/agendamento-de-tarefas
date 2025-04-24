e779e1e509acafcf74efb0f96fb0c006a39b7282
jonatas <jonatasgil92@gmail.com>
2025-04-23 21:48:32 -0400

    iniciada implementação de tela de tarefas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,54 @@
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
+import TarefaItem from '../../.expo/Tarefaitem';
+
 
 export default function Home(){
     return (
-        <View>
-            <Text>Home Page</Text>
+            <View style={styles.container}>
+              <View style={styles.cabecalho} >
+                <Text style={styles.titulo}>ABRIL / 2025</Text>
+                <View style={styles.icone}></View>
+
+
+            </View>
+            <View style={styles.body}>
+                <TarefaItem/>
+            </View>
         </View>
     );
 }
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    cabecalho:{
+        backgroundColor: 'blue',
+        width: '100%',
+        height: 60,
+        flexDirection: 'row',
+        justifyContent:'center',
+        alignItems:'center'
+    
+    
+    },
+    titulo:{
+        Color: 'white',
+        fontSize:25,
+        fontWeight:'bold',
+        textAlign:'center'
+        
+    },
+    icone:{
+        backgroundColor:'gray',
+        width:40,
+        height: 40,
+        borderRadius:20,
+        position:'absolute',
+        right:15
+    },
+    body:{
+
+    }
+
+
+});
